391dbc40f864906e346787e2a7998ed0b7d56350
angycarrillo <angelacruz200206@gmail.com>
2024-03-05 11:46:05 -0600

    Cmmit05/03

---
+++ src/main/java/com/appwithspring/demo/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.appwithspring.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("details")
+    public String details(){
+        return "details";
+    }
+}
